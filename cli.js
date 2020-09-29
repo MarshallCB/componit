0d4273ba34cbb1037dfa76ea19df551e893c2dc1
Marshall Brandt <MarshallCB@users.noreply.github.com>
2020-09-29 17:33:40 -0500

    improved documentation for previous changes

--- cli.js
+++ cli.js
@@ -10,9 +10,9 @@ sade('componit [input] [output]', true)
 .version(pkg.version)
 .describe(pkg.description)
 .example('components www/components --watch')
-.example('source public/components -w -l')
+.example('source public/components -w -d')
 .option('-w, --watch', 'Watch source directory and rebuild on changes')
-.option('--debug, -d', 'Debug (disable minification)', false)
+.option('--debug, -d', 'Debug mode - disable minification', false)
 .action((input, output, opts) => {
   let componit = new Componit(input, output, opts.debug)
   if(opts.watch){
